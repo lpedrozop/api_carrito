c8a6c979746ae52e1418664d119bd91518950e3b
lucas pedrozo <lukspedrozo@gmail.com>
2022-10-25 18:20:22 -0500

    update done

--- src/config.js
+++ src/config.js
@@ -2,12 +2,7 @@ import {config} from "dotenv";
 
 config()
 
-export const PORT = 3000
+export const PORT = 8080
 
-export const DB_USER = 'b82cc5a3579ef9'
-export const DB_PASSWORD = 'a8609492'
-export const DB_HOST = 'us-cdbr-east-06.cleardb.net'
-export const DB_DATABASE = 'heroku_66629adcf6a3cf0'
-export const DB_PORT = 3306
 
 
