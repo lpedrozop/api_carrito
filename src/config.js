04690a77f6c521601c1f120c2c5513fbddf301a4
lucas pedrozo <lukspedrozo@gmail.com>
2022-10-25 18:03:30 -0500

    update done

--- src/config.js
+++ src/config.js
@@ -2,11 +2,12 @@ import {config} from "dotenv";
 
 config()
 
-export const PORT = process.env.PORT || 3000
+export const PORT = 3000
 
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '123456Lp'
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'api_inventario'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_USER = 'b82cc5a3579ef9'
+export const DB_PASSWORD = 'a8609492'
+export const DB_HOST = 'us-cdbr-east-06.cleardb.net'
+export const DB_DATABASE = 'heroku_66629adcf6a3cf0'
+export const DB_PORT = 3306
+
 
