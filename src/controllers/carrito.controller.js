4de75076ee44a899bd34a1fe0dd10073a6cca627
Lucas M. Pedrozo P <93496498+lpedrozop@users.noreply.github.com>
2022-11-24 20:43:42 -0500

    Update

--- src/controllers/carrito.controller.js
+++ src/controllers/carrito.controller.js
@@ -13,21 +13,11 @@ export const getcarrito = async (req, res) => {
 
 export const getcarrito_ind = async (req, res) => {
   try{
-      const [items] = req.body
-      console.log(items)
-      if(items.affectedRows <=0){
-          return res.status(404).json({
-              message:'Agg productos a tu carrito'})
-      }
-      else {
-          items.forEach(items =>{
-              const [rows] = pool.query('SELECT id, title, author, category, detail, amount, sale_price, url_i FROM inventario WHERE id = ?', [items.id])
-              if (rows.length <= 0 ) return res.status(404).json({
-                  message:'Not found'
-              })
-              else{res.json(rows)}
-
-          })}
+      const [rows] = await pool.query('SELECT id, title, author, category, detail, amount, sale_price, url_i FROM inventario WHERE id = ?', [req.params.id])
+      if (rows.length <= 0 ) return res.status(404).json({
+          message:'Not found'
+      })
+      res.json(rows[0])
   }
    catch (error) {
       return res.status(500).json({message: 'Error de conexión'})
